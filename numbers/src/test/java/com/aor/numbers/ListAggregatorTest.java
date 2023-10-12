3a697482a3fa1e994ac571546dfa9f733ac1b763
Alexandre Silva <asilva1604@outlook.com>
2023-10-12 17:10:30 +0100

    Step 4 complete

--- numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -45,4 +45,13 @@ public class ListAggregatorTest {
 
         Assertions.assertEquals(4, distinct);
     }
+
+    @Test
+    public void max_bug_72363() {
+        list = Arrays.asList(-1, -4, -5);
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(-1, max);
+    }
 }
